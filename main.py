c20c53b427c74db03d3dd109f6d1fe4c1ad7e84c
SPablo2191 <pablosandoval2191@gmail.com>
2024-11-09 11:33:36 -0300

    Revert "feat: prueba de checkout"
    This reverts commit c557d08e0bc78d2b16b9674c8d26e3cc69b54947.

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-from datetime import datetime, timedelta
-import random
-
-# Obtener la hora actual
-current_date = datetime.now()
-
-# Calcular las 9 AM de hoy y las 9 AM de ayer
-today_at_9am = current_date.replace(hour=9, minute=0, second=0, microsecond=0)
-yesterday_at_9am = today_at_9am - timedelta(days=1)
-
-# Generar una fecha aleatoria entre las 9 AM de ayer y las 9 AM de hoy
-random_date = yesterday_at_9am + (today_at_9am - yesterday_at_9am) * random.random()
-
-# Mostrar la fecha en formato ISO
-print(random_date.isoformat())
+print("adios")
+print("asdasdsadasd")
